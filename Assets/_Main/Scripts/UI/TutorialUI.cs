52b229987c7aa5cc080860c05bff618a45f1b343
DUSDJ <zxc29321@gmail.com>
2023-12-12 03:11:36 +0900

    ㅇ튜토리얼 클리어/스킵시 게임시작

---
+++ Assets/_Main/Scripts/UI/TutorialUI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace FC
+{
+    public class TutorialUI : MonoBehaviour
+    {
+
+        public void OnClickTutorialEnd()
+        {
+            GameManager.Instance.StartGame();
+        }
+    }
+}
